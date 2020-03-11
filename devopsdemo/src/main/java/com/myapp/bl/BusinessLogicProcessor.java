ca4f44e4b27afac5210948da1895ee10b32f8725
mahendrahotte <mahendra.hotte@siemens.com>
2020-03-11 12:38:34 +0530

    Changed to use Seln Sort

--- devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
+++ devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
@@ -133,7 +133,7 @@ public class BusinessLogicProcessor {
 		 ///////////////////////////////// ----- Selection sort algorithm starts
 		  
 		 // Outer loop - need n-1 iteration to sort n elements 
-		 /*
+		 
 		 for(int i=0; i<arr.length-1; i++){           /// this line is the correct logic 
 		 // for(int i=1; i<arr.length-1; i++) {        /// uncomment this line to introduce a logical bug
 		 
@@ -150,9 +150,10 @@ public class BusinessLogicProcessor {
 		  
 		 } 
 		 ///////////////////////////////////// ----- Selection sort algorithm ends
-		 */
+		 
 		
 		////////////////////////////////////////////// Bubble sort logic starts
+		 /*
 		int n = arr.length;
 		int k;
 		for (int m = n; m >= 0; m--) {
@@ -164,6 +165,7 @@ public class BusinessLogicProcessor {
 			}
 		}
 		//////////////////////////////////////////////// Bubble sort logic ends
+		 */
 		
 		// System.out.println("Sorted array >>"+ Arrays.toString(arr));
 		return arr;
