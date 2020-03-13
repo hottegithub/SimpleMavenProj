a0946f4ce13ecedff5d4edb8e7e2f1625cf6d337
mahendrahotte <mahendra.hotte@siemens.com>
2020-03-13 16:36:57 +0530

    Changed logic to bubble sort.

--- devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
+++ devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
@@ -133,7 +133,7 @@ public class BusinessLogicProcessor {
 		 ///////////////////////////////// ----- Selection sort algorithm starts
 		  
 		 // Outer loop - need n-1 iteration to sort n elements 
-		 
+		 /*
 		 for(int i=0; i<arr.length-1; i++){           /// this line is the correct logic 
 		 // for(int i=1; i<arr.length-1; i++) {        /// uncomment this line to introduce a logical bug
 		 
@@ -151,9 +151,9 @@ public class BusinessLogicProcessor {
 		 } 
 		 ///////////////////////////////////// ----- Selection sort algorithm ends
 		 
-		
+		*/
 		////////////////////////////////////////////// Bubble sort logic starts
-		 /*
+		 
 		int n = arr.length;
 		int k;
 		for (int m = n; m >= 0; m--) {
@@ -165,7 +165,7 @@ public class BusinessLogicProcessor {
 			}
 		}
 		//////////////////////////////////////////////// Bubble sort logic ends
-		 */
+		 
 		
 		// System.out.println("Sorted array >>"+ Arrays.toString(arr));
 		return arr;
