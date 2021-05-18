f63a8a9e355978c54bce044925d8e14effde5e38
mahendrahotte <mahendra.hotte@siemens.com>
2021-05-18 21:37:03 +0530

    Changed to singular

--- devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
+++ devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
@@ -133,7 +133,7 @@ public class BusinessLogicProcessor {
 		 ///////////////////////////////// ----- Selection sort algorithm starts
 		  
 		 // Outer loop - need n-1 iteration to sort n elements 
-		 
+		 /*
 		 for(int i=0; i<arr.length-1; i++){           /// this line is the correct logic 
 		 // for(int i=1; i<arr.length-1; i++) {        /// uncomment this line to introduce a logical bug
 		 
@@ -150,9 +150,9 @@ public class BusinessLogicProcessor {
 		  
 		 } 
 		 ///////////////////////////////////// ----- Selection sort algorithm ends
-		 
+		 */
 		
-		/*
+		
 		////////////////////////////////////////////// Bubble sort logic starts
 		 
 		int n = arr.length;
@@ -166,7 +166,7 @@ public class BusinessLogicProcessor {
 			}
 		}
 		//////////////////////////////////////////////// Bubble sort logic ends
-		*/
+		
 		
 		// System.out.println("Sorted array >>"+ Arrays.toString(arr));
 		return arr;
