269793fd46fa3ed4de89302c5ce01a0aba3106ca
mahendrahotte <mahendra.hotte@gmail.com>
2022-07-29 12:59:43 +0530

    Changed to Seln Sort

--- devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
+++ devopsdemo/src/main/java/com/myapp/bl/BusinessLogicProcessor.java
@@ -128,7 +128,7 @@ public class BusinessLogicProcessor {
 		// System.out.println("unsorted array before sorting : " + Arrays.toString(arr));
 
 		
-		/*
+		
 		///////////////////////////////// ----- Selection sort algorithm starts
 		  
 		 // Outer loop - need n-1 iteration to sort n elements 
@@ -149,8 +149,8 @@ public class BusinessLogicProcessor {
 		  
 		 } 
 		 ///////////////////////////////////// ----- Selection sort algorithm ends
-		*/
 		
+		/*
 		
 		////////////////////////////////////////////// Bubble sort logic starts
 		 
@@ -165,7 +165,7 @@ public class BusinessLogicProcessor {
 			}
 		}
 		//////////////////////////////////////////////// Bubble sort logic ends
-		
+		*/
 		
 		// System.out.println("Sorted array >>"+ Arrays.toString(arr));
 		return arr;
