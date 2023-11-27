e3aff7f7df712a1a030902c3088ceac1dbef9a94
z0041wcv <z0041wcv@129.214.135.28>
2023-11-27 12:02:37 +0530

    Commented out the code

--- devopsdemo/src/test/java/com/myapp/cucumber/CucumberTests.java
+++ devopsdemo/src/test/java/com/myapp/cucumber/CucumberTests.java
@@ -2,16 +2,14 @@ package com.myapp.cucumber;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "src/test/resources",
-		glue = "com.myapp.cucumber",
-		plugin = {"json:target/cucumber.json"}
-)
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+	//	features = "src/test/resources",
+	//	glue = "com.myapp.cucumber",
+	//	plugin = {"json:target/cucumber.json"}
+//)
 
 
 public class CucumberTests {
